bc299c037ad25bea50cddd239a5d4b759862164d
kevinbloomquist <rkevinbloom@gmail.com>
2017-08-19 12:59:39 -0600

    cleans up html in index and show views

--- books/resources/views/books/index.blade.php
+++ books/resources/views/books/index.blade.php
@@ -2,38 +2,34 @@
 
 @extends('layouts.app')
 @section('main')
-
-
-
-
   
-    @include('books.nav')
+  @include('books.nav')
 
       @if (Session::has('message'))
       <div class="alert alert-info">{{ Session::get('message') }}</div>
       @endif
+
     <div class= "row">
+
       @foreach($books as $book)
         <div class = "card col-md-3">
-          <img class= "card-image-top rounded mx-auto d-block" src = "{{$book->cover_url}}" width = "150rem" height = "200rem" alt = "Sweet Book Pic Goes Here!">
-        <div class="card-title text-center">
-            <a href = "{{route('books.show', $book)}}">
-              <span class ="card-title">{{$book->title}}</span>
-            </a> 
-        </div>
-        <form action="{{ url('books/'.$book->id) }}" method="POST">
-            {{ csrf_field() }}
-            {{ method_field('DELETE') }}
-            <button type="submit" class="btn btn-block">Delete
-            </button>
-            <a class="btn btn-block" href="{{ route('books.edit',$book) }}">Edit this book</a>
-        </form>
+            <img class= "card-image-top rounded mx-auto d-block" src = "{{$book->cover_url}}" width = "150rem" height = "200rem" alt = "Sweet Book Pic Goes Here!">
+            <div class="card-title text-center">
+                <a href = "{{route('books.show', $book)}}">
+                  <span class ="card-title">{{$book->title}}</span>
+                </a> 
+            </div>
+            <form action="{{ url('books/'.$book->id) }}" method="POST">
+                {{ csrf_field() }}
+                {{ method_field('DELETE') }}
+                <button type="submit" class="btn btn-block">Delete</button>
+                <a class="btn btn-block" href="{{ route('books.edit',$book) }}">Edit this book</a>
+            </form>
         </div>
       @endforeach
-      </div>
-      </div>
+    </div>
         <div class="pagination pagination-lg justify-content-center">
-          {{$books->links()}}
+        {{$books->links()}}
         </div>
 
 @endsection
--- books/resources/views/books/show.blade.php
+++ books/resources/views/books/show.blade.php
@@ -1,25 +1,29 @@
 @extends('layouts.app')
 @section('main')
-
-<div class= "card">
-<h1>{{$book->name}}</h1>
-<ul>
-	<!-- once built out change width/height to responsive values for final layout -->
-	<img src = "{{$book->cover_url}}" width = "300px" height = "400px" alt = "Sweet Book Pic Goes Here!">
-	<h2>Title: {{$book->title}}</h2>
-	<h3>Author: {{$book->author}}</h3>
-	<p>Publication Date: {{$book->pub_date}}</p>
-	<p>DB id: {{$book->id}}</p>
-
-
-</ul>
-    <form action="{{ url('books/'.$book->id) }}" method="POST">
-        {{ csrf_field() }}
-        {{ method_field('DELETE') }}
-        <button type="submit" class="btn btn-block">Delete
-        </button>
-        <a class="btn btn-block" href="{{ route('books.edit',$book) }}">Edit this book</a>
-    </form>
+<div class="row">
+    <div class= "card">
+        <div class="card-block justify-content-center">
+            <h1>{{$book->name}}</h1>
+            <ul>
+            	<!-- once built out change width/height to responsive values for final layout -->
+            	<img src = "{{$book->cover_url}}" width = "300px" height = "400px" alt = "Sweet Book Pic Goes Here!">
+            	<h2>Title: {{$book->title}}</h2>
+            	<h3>Author: {{$book->author}}</h3>
+            	<p>Publication Date: {{$book->pub_date}}</p>
+            	<p>DB id: {{$book->id}}</p>
+            </ul>
+        </div>
+        <div class="card-block justify-content-center">
+        <form action="{{ url('books/'.$book->id) }}" method="POST">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <button type="submit" class="btn btn-block">Delete
+            </button>
+            <a class="btn btn-block" href="{{ route('books.edit',$book) }}">Edit this book</a>
+        </form>
+        </div>
+    </div>
+    </div>
 </div>
     
 @endsection
